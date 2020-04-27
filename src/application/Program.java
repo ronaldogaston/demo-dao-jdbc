9dd917ae3e718ae691fe08de3f2e72415ea4ea26
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-04-27 11:37:08 -0300

    Classe Departamento

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1, "Livros");
+		System.out.println(obj);
+
+	}
+
+}
