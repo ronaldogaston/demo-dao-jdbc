60846d1cd52d8a132c6cc81493fdbc08e053bde6
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-04-27 11:44:07 -0300

    Classe Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Departamento obj = new Departamento(1, "Livros");
-		System.out.println(obj);
+		
+		Vendedor vendedor = new Vendedor(21, "Jéssica Closs", "jehcloss@gmail.com", new Date(), 2100.0, obj);
+		
+		System.out.println(vendedor);
 
 	}
 
